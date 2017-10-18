4d5a8f0d130fdb93df8df26dce2dc64e53d22549
wsalasg@eafit.edu.co <wsalasg@eafit.edu.com>
2017-10-18 15:03:41 -0500

    Modifying config

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ const normalize = require('normalize-path');
 
 module.exports = {
 
-    ip: '127.0.0.1',
-    port: '3000',
-    rootPath: normalize(process.env.PWD),
-    baseURL: '/',
-    app: {
-        name: 'easy_games',
-    },
-    db: 'mongodb://127.0.0.1/easygames',
-    steamAPIKey: 'D190763820D0D332C6BE435753280A36'
-    
+  ip: '127.0.0.1',
+  port: '3001',
+  rootPath: normalize(process.env.PWD),
+  baseURL: '/',
+  app: {
+    name: 'easy_games',
+  },
+  db: 'mongodb://127.0.0.1/easygames',
+  steamAPIKey: 'D190763820D0D332C6BE435753280A36'
+
 };
